1fa1d61194c472d939ca7369d3f47349c7f6e2e1
miqueelino <joao.jnunes@mercadolivre.com>
2026-01-02 21:51:05 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/jpwork/workshopmongo/resources/UserResource.java
+++ src/main/java/com/jpwork/workshopmongo/resources/UserResource.java
@@ -55,5 +55,14 @@ public class UserResource {
 	    return ResponseEntity.noContent().build();
 	}
 	
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable("id") String id) {
+	    User obj = service.fromDTO(objDto);
+	    obj.setId(id);
+	    obj = service.update(obj);
+	    return ResponseEntity.noContent().build();
+	}
+	
 
 }
