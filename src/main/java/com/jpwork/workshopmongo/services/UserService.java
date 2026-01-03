554085c3f03b5a881868fd68c6a43adab754f1f1
miqueelino <joao.jnunes@mercadolivre.com>
2026-01-02 21:14:53 -0300

    obtendo um usuario por id

--- src/main/java/com/jpwork/workshopmongo/services/UserService.java
+++ src/main/java/com/jpwork/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jpwork.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jpwork.workshopmongo.domain.User;
 import com.jpwork.workshopmongo.repository.UserRepository;
+import com.jpwork.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,6 +21,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	
+	public User findById(String id) {
+			 Optional<User> obj = repo.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
 	}
 
 }
