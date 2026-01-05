0af1ec76c92dbe0da33038d4224f8688fc957c96
miqueelino <joao.jnunes@mercadolivre.com>
2026-01-05 15:53:19 -0300

    Obtendo post por id

---
+++ src/main/java/com/jpwork/workshopmongo/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.jpwork.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jpwork.workshopmongo.domain.Post;
+
+import com.jpwork.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+	
+	
+	@Autowired
+	private PostService service;
+
+	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Post> findById(@PathVariable("id") String id) {
+	   Post obj = service.findById(id);
+	    return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/jpwork/workshopmongo/services/PostService.java
@@ -0,0 +1,35 @@
+package com.jpwork.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jpwork.workshopmongo.domain.Post;
+import com.jpwork.workshopmongo.repository.PostRepository;
+import com.jpwork.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+
+
+	    private final PostRepository repo;
+
+	    @Autowired
+	    public PostService(PostRepository repo) {
+	        this.repo = repo;
+	    }
+
+	    public Post findById(String id) {
+	        if (id == null || id.isBlank()) {
+	            throw new IllegalArgumentException("ID do post não pode ser nulo ou vazio");
+	        }
+
+	        return repo.findById(id)
+	                .orElseThrow(() ->
+	                        new ObjectNotFoundException("Post não encontrado. Id: " + id)
+	                );
+	    }
+	}
+
+
+
