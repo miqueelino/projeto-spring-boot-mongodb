5a913a5199c6be06ad4a79794c596689b059017d
miqueelino <joao.jnunes@mercadolivre.com>
2026-01-06 16:00:11 -0300

    Consulta com varios criterios

--- src/main/java/com/jpwork/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jpwork/workshopmongo/resources/PostResource.java
@@ -1,7 +1,9 @@
 package com.jpwork.workshopmongo.resources;
 
+
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jpwork.workshopmongo.domain.Post;
+import com.jpwork.workshopmongo.resources.util.URL;
 import com.jpwork.workshopmongo.services.PostService;
 
 
@@ -30,12 +33,26 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    @GetMapping("/titlesearch")
-    public ResponseEntity<List<Post>> findByTitle(
-            @RequestParam(value = "text", defaultValue = "") String text) {
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 
+		text = URLDecoder.decode(text, StandardCharsets.UTF_8);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+    
+    
+    
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch (
+    	@RequestParam(value = "text", defaultValue = "") String text,
+    	@RequestParam(value = "minDate", defaultValue = "") String minDate,
+    	@RequestParam(value = "maxDate", defaultValue = "") String maxDate
+    ) {
         text = URLDecoder.decode(text, StandardCharsets.UTF_8);
-        List<Post> list = service.findByTitle(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date(0L));
+        List<Post> list = service.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/jpwork/workshopmongo/resources/util/URL.java
+++ src/main/java/com/jpwork/workshopmongo/resources/util/URL.java
@@ -2,6 +2,9 @@ package com.jpwork.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -13,5 +16,16 @@ public class URL {
 			return "";
 		}
 
+	}
+	
+	public static Date convertDate (String textDate, Date defaultDate) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (Exception e) {
+			return defaultDate;
+		}
+		
 	}
 }
